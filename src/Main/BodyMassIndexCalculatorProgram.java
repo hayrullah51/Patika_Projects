54237a56cd4b44cfcb252a65e92fce6d945d2f67
Hayrullah DEDELER <h.dede515151@gmail.com>
2022-11-01 12:59:46 +0300

    1.11.22

---
+++ src/Main/BodyMassIndexCalculatorProgram.java
@@ -0,0 +1,22 @@
+package Main;
+
+import java.util.Scanner;
+
+public class BodyMassIndexCalculatorProgram {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please Enter Your Weight(as inche)...");
+        double weight = scan.nextDouble();
+
+        System.out.println("Please Enter Your Height(as meter)...");
+        double height = scan.nextDouble();
+
+        double result = weight/(height * height);
+
+        System.out.println("Your Body Mass Index : " + result);
+
+
+    }
+}
